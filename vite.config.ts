738eea694238c7ea0603f85c07ccbb28597f12b6
uamchugalo <fradebraga@gmail.com>
2025-01-31 14:10:33 -0300

    Fix: Update Vite configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/project/', // nome do seu repositório
+  base: '/fb-os/', // nome do seu repositório
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+  }
 });
